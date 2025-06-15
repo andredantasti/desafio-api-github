a70a1d2d0c3fc2be8c16888e57d074980516c80e
Andre Dantas <andremoreirati@gmail.com>
2025-06-15 15:08:40 -0300

    Atualização do projeto acrescentado novas informações ao perfil, eventos e mais informações do repositorio

--- src/javascript/indexCorrecao.js
+++ src/javascript/indexCorrecao.js
@@ -1,13 +1,17 @@
-
-import { baseUrl, repositoriesQuantity } from "./services/variables.js";
+import {
+  baseUrl,
+  repositoriesQuantity,
+  eventsQuantity,
+} from "./services/variables.js";
 import { getUser } from "./services/getUser.js";
 import { getRepositories } from "./services/getRepositories.js";
+import { getEvents } from "./services/getEvents.js";
 import { user } from "./objects/user.js";
 import { screen } from "./objects/screen.js";
 
 document.getElementById("btn-search").addEventListener("click", function () {
   const userName = document.getElementById("input-search").value;
-  if(validadeEmptyInput(userName)) return;
+  if (validadeEmptyInput(userName)) return;
   getUserData(userName);
 });
 
@@ -16,7 +20,7 @@ document.getElementById("input-search").addEventListener("keyup", function (e) {
   const key = e.which || e.keyCode; //acionando ação pelo teclado
   const isEnterKeyPressed = key === 13;
   if (isEnterKeyPressed) {
-   if(validadeEmptyInput(userName)) return;
+    if (validadeEmptyInput(userName)) return;
     getUserData(userName);
   }
 });
@@ -24,24 +28,25 @@ document.getElementById("input-search").addEventListener("keyup", function (e) {
 async function getUserData(userName) {
   const userResponse = await getUser(userName);
 
-  if(userResponse.message === "Not Found"){
+  if (userResponse.message === "Not Found") {
     screen.renderNotFound();
     return;
   }
 
   const repositoriesResponse = await getRepositories(userName);
 
+  const eventsResponse = await getEvents(userName);
+
   user.setInfo(userResponse);
   user.setRepositories(repositoriesResponse);
-  console.log(user);
+  user.setEvents(eventsResponse);
 
   screen.renderUser(user);
-
 }
 
-function validadeEmptyInput(userName){
-     if (userName.length === 0) {
-      alert("Preencha o campo com um nome de usuário do GitHub");
-      return true;
-    }
+function validadeEmptyInput(userName) {
+  if (userName.length === 0) {
+    alert("Preencha o campo com um nome de usuário do GitHub");
+    return true;
+  }
 }
--- src/javascript/objects/screen.js
+++ src/javascript/objects/screen.js
@@ -7,21 +7,55 @@ const screen = {
                      }" alt="Foto perfil usuário">    <div class= "data">
                      <h1>${user.name ?? "Não possui nome cadastrado"}</h1>
                  <p>${user.bio ?? "Não possui Bio cadastrada"}</p>
+                 <p>  <i class="fas fa-user"> </i> ${
+                   user.followers
+                 } Followers </p> 
+                 <p> <i class="fas fa-user"></i> ${
+                   user.following
+                 } Following </p>
                  </div>
                  </div>`;
 
     let repositoriesItens = "";
-    user.repositories.forEach(repo => repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`);
+    user.repositories.forEach(
+      (repo) =>
+        (repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name} <p>
+      <span><i class="fa-solid fa-code-fork"></i> ${repo.forks}</span> 
+      <span><i class="fa-solid fa-star fa-" style="color: #FFD43B;"></i> ${repo.stargazers_count}</span> 
+      <span>👀${repo.watchers}</span> 
+      <span>🧑‍🏫${repo.language}</span></p></a>
+      </li>`)
+    );
 
-      if(user.repositories.length > 0){
-         this.userProfile.innerHTML += `<div class="repositories section">
+    if (user.repositories.length > 0) {
+      this.userProfile.innerHTML += `<div class="repositories section">
          <h2>Repositórios</h2>
-         <ul>${repositoriesItens}</ul><div/>`
-      }
-    },
-    renderNotFound(){
-        this.userProfile.innerHTML = "<h3>Usuário não Encontrado</h3>"
+         <ul>${repositoriesItens}</ul><div/>`;
     }
-  }
+
+    let eventsItens = "";
+
+    user.events.forEach((event) => {
+      if (event.type === "PushEvent" || "CreateEvent") {
+        let repoName = event.repo.name;
+
+        if (event.type === "PushEvent") {
+          let message = event.payload.commits[0].message;
+          eventsItens += `<li><strong>${repoName}</strong> - ${message}</li>`;
+        } else {
+          eventsItens += `<li><strong>${repoName}</strong> - Sem mensangem de commit</li>`;
+        }
+      }
+    });
+
+    this.userProfile.innerHTML += `<div class="events section">
+      <h2>Eventos</h2>
+      <ul>${eventsItens}</ul>
+      </div>`;
+  },
+  renderNotFound() {
+    this.userProfile.innerHTML = "<h3>Usuário não Encontrado</h3>";
+  },
+};
 
 export { screen };
